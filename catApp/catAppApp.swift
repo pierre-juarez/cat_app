165c3fc76602c0c85e01c1e0aa58beae93e1a98c
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-11-20 00:34:17 -0500

    Initial Commit

---
+++ catApp/catAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  catAppApp.swift
+//  catApp
+//
+//  Created by Pierre Juarez on 20/11/22.
+//
+
+import SwiftUI
+
+@main
+struct catAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
